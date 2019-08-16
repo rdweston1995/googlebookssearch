ad216e685f68e9c9f535553dfa6cadc00e137288
Robert <robert.d.weston@gmail.com>
2019-08-15 23:21:07 -0700

    Heroku init

---
+++ client/build/precache-manifest.e4fc9c99d4b92cf4d0978152d4b71bf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bac2470c591509fe64a567c535b4b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "118dc3efbe8bcc5f44df",
+    "url": "/static/css/main.106f3c77.chunk.css"
+  },
+  {
+    "revision": "0fd1d0f953d87df3586b",
+    "url": "/static/js/2.63763eeb.chunk.js"
+  },
+  {
+    "revision": "118dc3efbe8bcc5f44df",
+    "url": "/static/js/main.81eb431b.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
